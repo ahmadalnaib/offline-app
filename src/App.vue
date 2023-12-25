6ca7dfd4a1ec1d0fa7b529990d25513207c66504
ahmedhabeeb73 <alnaib888@gmail.com>
2023-12-25 20:29:10 +0100

    add editor

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="flex w-screen h-screen text-gray-700">
+    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+      <!-- Sidebar -->
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="flex flex-col flex-grow">
+      <!-- Main Content -->
+      <div class="flex flex-col flex-grow overflow-auto">
+        <editor-content :editor="editor" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+const editor = useEditor({
+  content: 'Some Content',
+  extensions: [
+    StarterKit,
+  ],
+  editorProps: {
+    attributes: {
+      class: "prose my-6 mx-auto focus:outline-none"
+    }
   }
+})
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import './app.css';
 
 createApp(App).mount('#app')
